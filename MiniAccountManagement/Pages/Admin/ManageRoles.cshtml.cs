ae71d2f6f2bac0b8b4ca5882ab420c5a802207f2
masumkazibd <masum.kazi.hantmk@gmail.com>
2025-07-01 03:32:47 +0800

    create role by store procedure

--- MiniAccountManagement/Pages/Admin/ManageRoles.cshtml.cs
+++ MiniAccountManagement/Pages/Admin/ManageRoles.cshtml.cs
@@ -1,8 +1,10 @@
+using Dapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using System.Data;
 
 namespace MiniAccountManagement.Data.Admin
 {
@@ -10,14 +12,33 @@ namespace MiniAccountManagement.Data.Admin
     public class ManageRolesModel : PageModel
     {
         private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IDbConnection _dbConnection;
         public List<IdentityRole> Roles { get; set; }
-        public ManageRolesModel(RoleManager<IdentityRole> roleManager)
+        public ManageRolesModel(RoleManager<IdentityRole> roleManager, IDbConnection dbConnection)
         {
             _roleManager = roleManager;
+            _dbConnection = dbConnection;
         }
         public async Task OnGetAsync()
         {
             Roles = await _roleManager.Roles.ToListAsync();
+        }
+        public async Task<IActionResult> OnPostCreateRole(string RoleName)
+        {
+            var parameters = new DynamicParameters();
+            parameters.Add("@RoleName", RoleName);
+            await _dbConnection.ExecuteAsync("CreateRole", parameters, commandType: CommandType.StoredProcedure);
+
+            return RedirectToPage();
+        }
+
+        public async Task<IActionResult> OnPostDeleteRole(string RoleId)
+        {
+            var parameters = new DynamicParameters();
+            parameters.Add("@RoleId", RoleId);
+            await _dbConnection.ExecuteAsync("DeleteRole", parameters, commandType: CommandType.StoredProcedure);
+
+            return RedirectToPage();
         }
     }
 }
