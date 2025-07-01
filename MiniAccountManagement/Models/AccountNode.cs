9331d2bc8cf30f9050ad660de45ce2937b09630a
masumkazibd <masum.kazi.hantmk@gmail.com>
2025-07-01 14:24:13 +0800

    account models for chart of account

---
+++ MiniAccountManagement/Models/AccountNode.cs
@@ -0,0 +1,7 @@
+﻿namespace MiniAccountManagement.Models
+{
+    public class AccountNode : Account
+    {
+        public List<AccountNode> Children { get; set; } = new List<AccountNode>();
+    }
+}
