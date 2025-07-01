d53d7dda0b719bc84c20c217166d024d9a24bba7
masumkazibd <masum.kazi.hantmk@gmail.com>
2025-07-01 13:31:32 +0800

    manage user role by store procedure

--- MiniAccountManagement/Pages/Admin/ManageUsers.cshtml.cs
+++ MiniAccountManagement/Pages/Admin/ManageUsers.cshtml.cs
@@ -1,8 +1,11 @@
+using Dapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using System.Data;
+using System.Data.Common;
 
 namespace MiniAccountManagement.Data.Admin
 {
@@ -10,31 +13,46 @@ namespace MiniAccountManagement.Data.Admin
     public class ManageUsersModel : PageModel
     {
         private readonly UserManager<IdentityUser> _userManager;
-
+        private readonly IDbConnection _dbConnection;
+        private readonly RoleManager<IdentityRole> _roleManager;
         public List<UserWithRoles> Users { get; set; }
-
-        public ManageUsersModel(UserManager<IdentityUser> userManager)
+        public List<IdentityRole> Roles { get; set; }
+        public ManageUsersModel(UserManager<IdentityUser> userManager, 
+                                IDbConnection connection, 
+                                RoleManager<IdentityRole> roleManager)
         {
             _userManager = userManager;
+            _dbConnection = connection;
+            _roleManager = roleManager;
         }
 
         public async Task OnGetAsync()
         {
             var users = await _userManager.Users.ToListAsync();
-
+            Roles = await _roleManager.Roles.ToListAsync();
             Users = new List<UserWithRoles>();
 
             foreach (var user in users)
             {
-                var roles = await _userManager.GetRolesAsync(user);
+                var userRoles = await _userManager.GetRolesAsync(user);
                 Users.Add(new UserWithRoles
                 {
                     Id = user.Id,
                     UserName = user.UserName,
                     Email = user.Email,
-                    Roles = roles.ToList()
+                    Roles = userRoles.ToList()
                 });
             }
+        }
+        public async Task<IActionResult> OnPostAssignUserRole(string UserId, string RoleId)
+        {
+            var parameters = new DynamicParameters();
+            parameters.Add("@UserId", UserId);
+            parameters.Add("@RoleId", RoleId);
+
+            await _dbConnection.ExecuteAsync("AssignUserRole", parameters, commandType: CommandType.StoredProcedure);
+
+            return RedirectToPage();
         }
     }
 
