aeba6611f6bd9a825b26a78f08f3f888f69defbe
masumkazibd <masum.kazi.hantmk@gmail.com>
2025-07-02 02:07:53 +0800

    view model for role rights

---
+++ MiniAccountManagement/Models/ManageRolePermissionsViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace MiniAccountManagement.Models
+{
+    public class ManageRolePermissionsViewModel
+    {
+        public string RoleId { get; set; }
+        public string RoleName { get; set; }
+
+        public List<string> AvailableModules { get; } = new List<string>
+    {
+        "Dashboard",
+        "ChartOfAccounts",
+        "VoucherEntry",
+        "UserManagement",
+        "Reports"
+    };
+        public List<string> AssignedModules { get; set; } = new List<string>();
+    }
+}
