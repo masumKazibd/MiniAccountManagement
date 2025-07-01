de89ca535c06eba3d3ae7c33bf9fd1cfff384ae2
masumkazibd <masum.kazi.hantmk@gmail.com>
2025-07-02 04:11:41 +0800

    extend authorization requirement

---
+++ MiniAccountManagement/Authorization/ModulePermissionHandler.cs
@@ -0,0 +1,40 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Authorization;
+using System.Data;
+using System.Security.Claims;
+
+namespace MiniAccountManagement.Authorization
+{
+
+    public class ModulePermissionHandler : AuthorizationHandler<ModulePermissionRequirement>
+    {
+        private readonly IDbConnection _dbConnection;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public ModulePermissionHandler(IDbConnection connection, IHttpContextAccessor httpContextAccessor)
+        {
+            _dbConnection = connection;
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        protected override async Task HandleRequirementAsync(
+            AuthorizationHandlerContext context,
+            ModulePermissionRequirement requirement)
+        {
+            var userId = context.User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            if (string.IsNullOrEmpty(userId))
+            {
+                return;
+            }
+
+            var parameters = new { UserId = userId, ModuleName = requirement.ModuleName };
+            var hasPermission = await _dbConnection.ExecuteScalarAsync<bool>("sp_CheckUserPermission", parameters, commandType: CommandType.StoredProcedure);
+
+            if (hasPermission)
+            {
+                context.Succeed(requirement);
+            }
+        }
+    }
+}
---
+++ MiniAccountManagement/Authorization/ModulePermissionRequirement.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace MiniAccountManagement.Authorization
+{
+    public class ModulePermissionRequirement : IAuthorizationRequirement
+    {
+        public string ModuleName { get; set; }
+        public ModulePermissionRequirement(string moduleName)
+        {
+            ModuleName = moduleName;
+        }
+    }
+}
