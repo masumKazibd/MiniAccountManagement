19e751987b5e05c981a367ce662401f4df38f6e3
masumkazibd <masum.kazi.hantmk@gmail.com>
2025-07-03 14:06:11 +0800

    added side panel for menu

--- MiniAccountManagement/wwwroot/js/site.js
+++ MiniAccountManagement/wwwroot/js/site.js
@@ -2,3 +2,13 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+window.addEventListener('DOMContentLoaded', event => {
+    // Toggle the side navigation
+    const sidebarToggle = document.body.querySelector('#sidebarToggle');
+    if (sidebarToggle) {
+        sidebarToggle.addEventListener('click', event => {
+            event.preventDefault();
+            document.body.classList.toggle('sb-sidenav-toggled');
+        });
+    }
+});
